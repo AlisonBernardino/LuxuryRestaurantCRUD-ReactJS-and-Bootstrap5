66d2fdb95c3271161d92879be6ac8a59e7f99b9d
Alison Bernardino <58011286+AlisonBernardino@users.noreply.github.com>
2022-03-27 16:21:48 +0100

    CRUD system styles and functionalities added

--- luxury-restaurant-crud/src/App.js
+++ luxury-restaurant-crud/src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+let initialState = [
+  {
+    id: 1,
+    description: '5-star plaza Restaurant'
+  },
+  {
+    id: 2,
+    description: 'Blue Hall Restaurant'
+  },
+];
+
 function App() {
+  const [restaurants, setRestaurants] = useState(initialState)
+
+  // Making an "Add" function through the pure JS method
+  function addRestaurant(event) {
+    event.preventDefault();
+
+    const restaurant = {
+      id: document.getElementById('idArea').value,
+      description: document.getElementById('descriptionArea').value,
+    }
+    setRestaurants([...restaurants, { ...restaurant }]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form className="row g-3">
+        <div className="col-md-6">
+          <label for="inputEmail4" className="form-label">ID: </label>
+          <input id='idArea' type="text" className="form-control" />
+        </div>
+        <div className="col-md-6">
+          <label for="inputEmail4" className="form-label">Title: </label>
+          <input id="descriptionArea" type="text" className="form-control" />
+        </div>
+        <div class="col-12">
+          <button className="btn btn-success" onClick={addRestaurant}> Add </button>
+        </div>
+      </form>
+      <div className='mt-3'>
+        {restaurants.map((restaurant) => (
+          <div key={restaurant.id} className="card mb-2 shadow">
+            <div className="card-body">
+              <p className="card-text"> {restaurant.id} - {restaurant.description} </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
