a985c23b6ae4515040bcba00f6881e2d7a4699ba
Alison Bernardino <58011286+AlisonBernardino@users.noreply.github.com>
2022-04-01 12:57:19 +0100

    Edit functions added

--- luxury-restaurant-crud/src/components/RestaurantForm.js
+++ luxury-restaurant-crud/src/components/RestaurantForm.js
@@ -1,11 +1,20 @@
 import { useState, useEffect } from 'react';
 
+const startingRestaurant = {
+    id: 0,
+    title: '',
+    rating: 0,
+    description: ''
+}
+
 export default function RestaurantForm(props) {
-    const [restaurant, setRestaurant] = useState({});
+    const [restaurant, setRestaurant] = useState(setCurrentRestaurant());
 
     useEffect(() => {
-        console.log("useEffect works!");
-    }, []);
+        if (props.selectedRestaurant.id !== 0) {
+            setRestaurant(props.selectedRestaurant);
+        }
+    }, [props.selectedRestaurant]);
 
     const handleInput = (item) => {
         const { mark, value } = item.target;
@@ -13,31 +22,55 @@ export default function RestaurantForm(props) {
         setRestaurant({ ...restaurant, [mark]: value });
     }
 
+    const handleCancelOperation = (item) => {
+        item.preventDefault();
+
+        setRestaurant(startingRestaurant);
+    }
+
+    function setCurrentRestaurant() {
+        if (props.selectedRestaurant !== 0) {
+            return props.selectedRestaurant;
+        } else {
+            return startingRestaurant;
+        }
+    }
+
     return (
-        <form className="row g-3">
-            <div className="col-md-6">
-                <label className="form-label">Title: </label>
-                <input mark="titleArea" id="titleArea" onChange={handleInput} type="text" className="form-control" value={restaurant.title} />
-            </div>
-            <div className="col-md-6">
-                <label className="form-label">Rating: </label>
-                <select mark="ratingArea" id="ratingArea" name="rating" onChange={handleInput} className="form-select" value={restaurant.rating}>
-                    <option defaultValue="0"> Choose an option... </option>
-                    <option value="1"> 01 (One) star </option>
-                    <option value="2"> 02 (Two) stars </option>
-                    <option value="3"> 03 (Three) stars </option>
-                    <option value="4"> 04 (Four) stars </option>
-                    <option value="5"> 05 (Five) stars </option>
-                </select>
-            </div>
-            <div className="col-md-12">
-                <label className="form-label">Description: </label>
-                <textarea mark="descriptionArea" id="descriptionArea" type="text" onChange={handleInput} className="form-control" value={restaurant.description}/>
-            </div>
-            <div className="col-12">
-                <button className="btn btn-success" onClick={props.addRestaurant}> Add </button>
-            </div>
-        </form>
+        <>
+        <hr/>
+        <h3> Luxury Restaurant - Establishments management </h3>
+        <hr/>
+            <form className="row g-3">
+                <div className="col-md-6">
+                    <label className="form-label">Title: </label>
+                    <input mark="titleArea" id="titleArea" onChange={handleInput} type="text" className="form-control" value={restaurant.title} />
+                </div>
+                <div className="col-md-6">
+                    <label className="form-label">Rating: </label>
+                    <select mark="ratingArea" id="ratingArea" name="rating" onChange={handleInput} className="form-select" value={restaurant.rating}>
+                        <option defaultValue="0"> Choose an option... </option>
+                        <option value="1"> 01 (One) star </option>
+                        <option value="2"> 02 (Two) stars </option>
+                        <option value="3"> 03 (Three) stars </option>
+                        <option value="4"> 04 (Four) stars </option>
+                        <option value="5"> 05 (Five) stars </option>
+                    </select>
+                </div>
+                <div className="col-md-12">
+                    <label className="form-label">Description: </label>
+                    <textarea mark="descriptionArea" id="descriptionArea" type="text" onChange={handleInput} className="form-control" value={restaurant.description} />
+                </div>
+                <div className="col-12">
+
+                    {restaurant.id === 0 ? (<button className="btn btn-success" onClick={props.addRestaurant}> Add new + </button>) :
+                        (<>
+                            <button className="btn btn-primary me-2" type="submit"> Save changes </button>
+                            <button className="btn btn-danger" onClick={handleCancelOperation}> Cancel </button>
+                        </>)}
+                </div>
+            </form>
+        </>
     )
 }
 
