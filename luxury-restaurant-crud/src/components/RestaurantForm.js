106cd722745c57bf658136c57e614f735fa91d15
Alison Fullstack Teacher <58011286+AlisonBernardino@users.noreply.github.com>
2022-04-30 20:19:39 -0300

    Restaurant CRUD completed

--- luxury-restaurant-crud/src/components/RestaurantForm.js
+++ luxury-restaurant-crud/src/components/RestaurantForm.js
@@ -1,21 +1,115 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 
 const startingRestaurant = {
-    ID : 0,
-    title: '',
-    rating: 0,
-    description: '',
+  ID: 0,
+  title: "",
+  rating: 0,
+  description: "",
 };
 
-export default function RestaurantForm(props){
-    const [place, setPlace] = useState(currentRestaurant());
+export default function RestaurantForm(props) {
+  const [place, setPlace] = useState(currentRestaurant());
 
-    useEffect(() => {
-        if(props.selectedRestaurant.ID !== 0) setPlace(props.selectedRestaurant);
-    }, [props.selectedRestaurant]);
+  useEffect(() => {
+    if (props.selectedRestaurant.ID !== 0) setPlace(props.selectedRestaurant);
+  }, [props.selectedRestaurant]);
 
-    const handleTextInput = (component) => {
-        const {element, value} = component.target;
-        setPlace({...place, [element]: value});
+  const handleTextInput = (component) => {
+    const { element, value } = component.target;
+    setPlace({ ...place, [element]: value });
+  };
+
+  const handleSubmission = (item) => {
+    item.preventDefault();
+
+    if (props.selectedRestaurant.ID !== 0) props.updateRestaurant(place);
+    else props.addRestaurant(place);
+
+    setPlace(startingRestaurant);
+  };
+
+  const handleCancellation = (element) => {
+    element.preventDefault();
+
+    // cancelarAtividade == cancelItem
+    props.cancelItem();
+
+    setPlace(startingRestaurant);
+  };
+
+  function configureCurrentRestaurant() {
+    if (props.selectedRestaurant.ID !== 0) {
+      return props.selectedRestaurant;
+    } else {
+      return startingRestaurant;
     }
+  }
+
+  return (
+    <>
+      <h1>Restaurant {place.ID !== 0 ? place.ID : ""}</h1>
+      <form className="row g-3" onSubmit={handleSubmission}>
+        <div className="col-md-6">
+          <label className="form-label"> Title: </label>
+          <input
+            name="title"
+            value={place.title}
+            onChange={handleTextInput}
+            id="title"
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <div className="col-md-6">
+          <label className="form-label"> Rating: </label>
+          <select
+            name="rating"
+            value={place.rating}
+            onChange={handleTextInput}
+            id="rating"
+            className="form-select"
+          >
+            <option defaultValue="0">Select an option...</option>
+            <option value="1"> 1 star </option>
+            <option value="2"> 2 stars </option>
+            <option value="3"> 3 stars </option>
+            <option value="4"> 4 stars </option>
+            <option value="5"> 5 stars (Max) </option>
+          </select>
+        </div>
+        <div className="col-md-12">
+          <label className="form-label"> Description: </label>
+          <textarea
+            name="description"
+            value={place.description}
+            onChange={handleTextInput}
+            id="description"
+            type="text"
+            className="form-control"
+          />
+          <hr />
+        </div>
+        <div className="col-12 mt-0">
+          {place.ID === 0 ? (
+            <button className="btn btn-primary" type="submit">
+              <i className="fas fa-plus me-2">Add</i>
+            </button>
+          ) : (
+            <>
+              <button className="btn btn-success me-2" type="submit">
+                <i className="fas fa-plus me-2"></i>
+                Save
+              </button>
+              <button className='btn btn-warning'
+              onClick={handleCancellation}
+              >
+                  <i className='fas fa-plus me-2'></i>
+                  Cancel
+              </button>
+            </>
+          )}
+        </div>
+      </form>
+    </>
+  );
 }
---
+++ luxury-restaurant-crud/src/components/RestaurantList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Restaurant from "./Restaurant";
+
+export default function RestaurantsList(props) {
+  return (
+    <div className="mt-3">
+      {props.restaurants.map((restaurant) => (
+        <Restaurant
+          key={restaurant.id}
+          restaurant={restaurant}
+          removeRestaurant={props.removeRestaurant}
+          editRestaurant={props.editRestaurant}
+        />
+      ))}
+    </div>
+  );
+}
